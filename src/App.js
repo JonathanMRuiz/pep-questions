d121079e6afae0741a77a3ab82c1b527ce8678ca
jonathanmruiz <jonimrz@yahoo.com>
2022-06-06 16:56:19 -0300

    QPCForm Recreation

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Sections from "./pages/sections";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="container">
+      <Sections />
+    </div>
+  );
+};
+
+export default App;
