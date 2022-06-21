ef2b7f947fca20c4ef94a9e6020a757c2478afea
JonathanMRuiz <jonimrz@yahoo.com>
2022-06-21 16:42:18 -0300

    6_21:getQuestions and modals

--- src/pages/sections/index.js
+++ src/pages/sections/index.js
@@ -1,20 +1,56 @@
 import { Checkbox, Collapse, Form, Input, Radio, Select } from "antd";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { data } from "./data";
 const { Panel } = Collapse;
 const { Option } = Select;
 
+const getQuestion = (questions) => {
+  const result = [];
+  // 1. iterar por cada pregunta.
+
+  questions.forEach((question) => {
+    result.push(question);
+
+    // 2. Si tiene modalCode buscar ese modalCode.
+    const questionModal = data.form.modals.find(
+      (q) => q.modalCode === question.code
+    );
+
+    // si no tiene ningun modal asociado, seguimos con la
+    // proxima pregunta
+    if (!questionModal) {
+      return;
+    }
+
+    // 3. Comparar respuesas entre (answerOption, answerCheckOption) y modalValue.
+    const showModal =
+      questionModal.modalValue === question.answerOption ||
+      question.answerCheckOption?.includes(questionModal.modalValue);
+
+    // si no tengo que mostrar el modal, sigo
+    // con la proxima pregunta
+    if (!showModal) {
+      return;
+    }
+
+    // 4.Agregar modalsQuestions
+    const modalQuestions = getQuestion(questionModal.modalQuestions);
+
+    result.push(...modalQuestions);
+  });
+
+  return result;
+};
+
 const sections = data.form.sections.reduce((acc, section) => {
-  const questionFilter = data.form.questions.filter(
+  const sectionQuestions = data.form.questions.filter(
     (item) => item.sectionId === section.id
   );
-
   acc.push({
     sectionTitle: section.display,
     sectionId: section.id,
-    questions: questionFilter,
-    modalQuestions: data.form.modals,
+    questions: getQuestion(sectionQuestions),
   });
 
   return acc;
@@ -29,11 +65,9 @@ const sections = data.form.sections.reduce((acc, section) => {
 
 const Sections = () => {
   const [form] = Form.useForm();
-  //const [isChecked, setIsChecked] = useState(false);
   const [responseModal, setResponseModal] = useState([]);
-  const handleResponse = (e) => {
-    const { value, id } = e.target;
 
+  const handleResponse = (id, value) => {
     const isDuplicate = responseModal.some((q) => q.id === id);
 
     if (!isDuplicate) {
@@ -44,24 +78,23 @@ const Sections = () => {
       findResponse.value = value;
       setResponseModal(newArray);
     }
-
-    console.log(responseModal);
   };
 
   useEffect(() => {
-    const newArray = data.form.questions.reduce((acc, question) => {
-      acc.push({
-        id: question.id,
-        answerNumber: question.answerNumber,
-        answerOption: question.answerOption,
-        answerText: question.answerText,
-        answerDetailsText: question.answerDetailsText,
-        answerCheckOption: question.answerDetailsText,
-      });
-      return acc;
-    }, []);
+    const sectionsQuestions = sections.flatMap((section) => section.questions);
 
-    setResponseModal(newArray);
+    const arr = sectionsQuestions.map((question) => {
+      return {
+        id: question.id,
+        value:
+          question.answerText ||
+          question.answerOption ||
+          question.answerDetailsText ||
+          question.answerNumber ||
+          question.answerCheckOption,
+      };
+    });
+    setResponseModal(arr);
   }, []);
 
   return (
@@ -69,29 +102,37 @@ const Sections = () => {
       <Collapse accordion>
         {sections.map((section) => (
           <Panel header={section.sectionTitle}>
-            {section.questions.map((q) => (
-              <Form.Item label={q.question} key={q.id}>
-                {/* {q.answerType === "TEXT" && (
-                  <Input
-                    id={q.id}
-                    onChange={handleResponse}
-                    key={`${q.id}#answerDetailsText`}
-                    value={responseModal}
-                  />
-                )} */}
-                {/* {q.answerType === "CHECK_OPTIONS" && (
-                  <Checkbox id={q.id}>
-                    {q.options.map((o) => (
-                      <span>{o.display}</span>
-                    ))}
-                  </Checkbox>
-                )} */}
-                {q.answerType === "OPTIONS" && (
-                  <>
+            {section.questions.map((q) => {
+              const findValue = responseModal.find((r) => r.id === q.id)?.value;
+
+              return (
+                <Form.Item label={q.question}>
+                  {q.answerType === "TEXT" && (
+                    <Input
+                      id={q.id}
+                      onChange={(e) => handleResponse(q.id, e.target.value)}
+                      value={findValue}
+                    />
+                  )}
+                  {q.answerType === "CHECK_OPTIONS" && (
+                    <Checkbox.Group
+                      id={q.id}
+                      onChange={(values) => handleResponse(q.id, values)}
+                      value={findValue}
+                    >
+                      {q.options.map((o) => (
+                        <Checkbox id={q.id} value={o.code}>
+                          <span>{o.display}</span>
+                        </Checkbox>
+                      ))}
+                    </Checkbox.Group>
+                  )}
+                  {q.answerType === "OPTIONS" && (
                     <Radio.Group
                       id={q.id}
                       aria-label={q.question || q.caption}
-                      onChange={handleResponse}>
+                      value={findValue}
+                    >
                       {q.options.map((o) => (
                         <Radio
                           id={q.id}
@@ -99,22 +140,29 @@ const Sections = () => {
                             o.display
                           }`.toLowerCase()}
                           value={o.code}
-                          onChange={handleResponse}>
+                          onChange={(e) => handleResponse(q.id, e.target.value)}
+                        >
                           {o.display}
                         </Radio>
                       ))}
                     </Radio.Group>
-                  </>
-                )}
-                {/* {q.answerType === "DROPDOWN" && (
-                  <Select placeholder='Dropdown...'>
-                    {q.options.map((o) => (
-                      <Option id={q.id} onChange={handleResponse}></Option>
-                    ))}
-                  </Select>
-                )} */}
-              </Form.Item>
-            ))}
+                  )}
+                  {q.answerType === "DROPDOWN" && (
+                    <Select
+                      value={findValue}
+                      id={q.id}
+                      onChange={(values) => handleResponse(q.id, values)}
+                    >
+                      {q.options.map((o) => (
+                        <Option id={q.id} value={o.code}>
+                          {o.display}
+                        </Option>
+                      ))}
+                    </Select>
+                  )}
+                </Form.Item>
+              );
+            })}
           </Panel>
         ))}
       </Collapse>
